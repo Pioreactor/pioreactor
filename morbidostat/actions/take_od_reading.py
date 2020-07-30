a83c6cf0d076ae258fdf14ddef1f9f234aff11b8
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-07-30 13:14:31 -0400

    try low pass

---
+++ morbidostat/actions/take_od_reading.py
@@ -0,0 +1,35 @@
+# take_optical_density
+
+# remove waste
+import time
+import configparser
+import click
+from  paho.mqtt import subscribe, publish
+
+TOPIC = "morbidostat/IR1_low_pass"
+
+@click.command()
+def take_optical_density():
+
+
+    click.echo(click.style("starting take_optical_density", fg='green'))
+    publish.single("morbidostat/log", "starting take_optical_density")
+
+    try:
+        result = subscribe.simple(TOPIC, keepalive=10).payload.decode(encoding='UTF-8')
+    except Exception as e:
+        click.echo(str(e))
+        return
+
+    click.echo(click.style("   %s" % result, fg='yellow'))
+    publish.single("morbidostat/log", "take_optical_density read %s" % result)
+
+    return
+
+
+
+if __name__ == '__main__':
+    try:
+        take_optical_density()
+    except Exception as e:
+        click.echo(str(e))
