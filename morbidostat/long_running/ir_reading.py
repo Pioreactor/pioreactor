bca1f171621353e8d9aec77b4f8e4c90aa1c6738
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-07-31 14:01:02 -0400

    parameters for ADC

--- morbidostat/long_running/ir_reading.py
+++ morbidostat/long_running/ir_reading.py
@@ -1,13 +1,15 @@
-from morbidostat.utils.streaming import LowPassFilter
 from statistics import stdev, mean
 import configparser
 import time
+
 import click
 from adafruit_ads1x15.analog_in import AnalogIn
 import adafruit_ads1x15.ads1115 as ADS
 from  paho.mqtt import publish
 import board
 import busio
+
+from morbidostat.utils.streaming import LowPassFilter
 
 
 
@@ -16,13 +18,13 @@ config.read('config.ini')
 
 
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
+ads = ADS.ADS1115(i2c, gain=16, data_rate=8)
 chan = AnalogIn(ads, ADS.P0)
 
 sm = LowPassFilter(60, 0.0001, 1)
 
 while True:
-    time.sleep(1.0)
+    time.sleep(float(config['ir_sampling']['samples_per_second']))
     try:
         raw_signal = chan.voltage
         sm.update(raw_signal)
