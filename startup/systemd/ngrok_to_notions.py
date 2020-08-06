6a07f11a33d80bcaa13c26a7dfbc39d9de8dbd3b
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-06 09:24:23 -0400

    startup files - need a readme

---
+++ startup/systemd/ngrok_to_notions.py
@@ -0,0 +1,13 @@
+# may need to sudo pip3 install notion
+from notion.client import NotionClient
+import requests
+
+r = requests.get("http://localhost:4040/api/tunnels")
+public_url = r.json()['tunnels'][0]['public_url']
+
+
+notions_token = ""
+
+client = NotionClient(token_v2=notions_token)
+page = client.get_block("https://www.notion.so/camdp/RaspberryPis-d8dfbc52736d4bd4b4551cddad2ea38d#2d50a0cdd45443449d02385374f5fc5a")
+page.title = public_url
