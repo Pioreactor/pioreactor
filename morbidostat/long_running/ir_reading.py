3c1cb009f7195ff31125b496ec4b8894047dd287
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-10 10:01:10 -0400

    updates

--- morbidostat/long_running/ir_reading.py
+++ morbidostat/long_running/ir_reading.py
@@ -18,21 +18,27 @@ config.read('config.ini')
 
 
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c, data_rate=8)
+ads = ADS.ADS1115(i2c, data_rate=8, gain=8)
 chan = AnalogIn(ads, ADS.P0, ADS.P1)
+sampling_rate =  1/float(config['ir_sampling']['samples_per_second'])
 
-sm = LowPassFilter(200, 0.0001, 1)
+sm = LowPassFilter(200, 0.0001, sampling_rate)
 
+i = 0
 while True:
-    time.sleep(1./float(config['ir_sampling']['samples_per_second']))
+    time.sleep(sampling_rate)
     try:
         raw_signal = chan.voltage
         sm.update(raw_signal)
-        if sm.latest_reading is not None:
+        if sm.latest_reading is not None and i % 100 == 0
             publish.single("morbidostat/IR1_low_pass", sm.latest_reading)
-        publish.single("morbidostat/IR1_raw", raw_signal)
+            publish.single("morbidostat/IR1_raw", raw_signal)
         print(raw_signal, sm.latest_reading)
+        i+=1
 
+    except OSError as e:
+        # just pause
+        time.sleep(5.0)
     except Exception as e:
         publish.single("morbidostat/log", "ir_reading.py failed")
         raise e
