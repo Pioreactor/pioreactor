cf64390d23c12286a0a35fabbdc7690e2141ca97
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-13 11:12:39 -0400

    better name

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,19 +2,21 @@
 
 CREATE TABLE IF NOT EXISTS od_readings_raw
     timestamp              TEXT  NOT NULL
-    measurement_source     TEXT  NOT NULL
+    morbidostat_unit       TEXT  NOT NULL
     od_reading_v           REAL  NOT NULL
     experiment             TEXT  NOT NULL
 
 CREATE TABLE IF NOT EXISTS od_readings_filtered
     timestamp              TEXT  NOT NULL
-    measurement_source     TEXT  NOT NULL
+    morbidostat_unit       TEXT  NOT NULL
     od_reading_v           REAL  NOT NULL
     experiment             TEXT  NOT NULL
 
-CREATE TABLE IF NOT EXISTS dilution_events
+CREATE TABLE IF NOT EXISTS io_events
     timestamp              TEXT  NOT NULL
     experiment             TEXT  NOT NULL
     event                  TEXT  NOT NULL
     volume_change_ml       REAL  NOT NULL
+    morbidostat_unit       TEXT  NOT NULL
+
 
