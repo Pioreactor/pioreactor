145bf2b43cfd294f90c6746e3e0c732a7d6d6ef5
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-13 11:33:03 -0400

    more

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,22 +1,24 @@
 # create_tables.sql
 
-CREATE TABLE IF NOT EXISTS od_readings_raw
-    timestamp              TEXT  NOT NULL
-    morbidostat_unit       TEXT  NOT NULL
-    od_reading_v           REAL  NOT NULL
+CREATE TABLE IF NOT EXISTS od_readings_raw (
+    timestamp              TEXT  NOT NULL,
+    morbidostat_unit       TEXT  NOT NULL,
+    od_reading_v           REAL  NOT NULL,
     experiment             TEXT  NOT NULL
+)
 
-CREATE TABLE IF NOT EXISTS od_readings_filtered
-    timestamp              TEXT  NOT NULL
-    morbidostat_unit       TEXT  NOT NULL
-    od_reading_v           REAL  NOT NULL
+CREATE TABLE IF NOT EXISTS od_readings_filtered (
+    timestamp              TEXT  NOT NULL,
+    morbidostat_unit       TEXT  NOT NULL,
+    od_reading_v           REAL  NOT NULL,
     experiment             TEXT  NOT NULL
+)
 
-CREATE TABLE IF NOT EXISTS io_events
-    timestamp              TEXT  NOT NULL
-    experiment             TEXT  NOT NULL
-    event                  TEXT  NOT NULL
-    volume_change_ml       REAL  NOT NULL
+CREATE TABLE IF NOT EXISTS io_events (
+    timestamp              TEXT  NOT NULL,
+    experiment             TEXT  NOT NULL,
+    event                  TEXT  NOT NULL,
+    volume_change_ml       REAL  NOT NULL,
     morbidostat_unit       TEXT  NOT NULL
-
+)
 
