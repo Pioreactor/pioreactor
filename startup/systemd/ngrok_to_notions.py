1a1849f85564980d7b16dc633298717bae65018a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-14 11:30:15 -0400

    creds

--- startup/systemd/ngrok_to_notions.py
+++ startup/systemd/ngrok_to_notions.py
@@ -1,7 +1,8 @@
 # may need to sudo pip3 install notion
 from notion.client import NotionClient
 import requests
-import sleep
+import time
+import configparser
 
 r = requests.get("http://localhost:4040/api/tunnels")
 public_url = None
@@ -11,11 +12,11 @@ while public_url is None:
     try:
         public_url = r.json()['tunnels'][0]['public_url']
     except:
-        sleep(10)
+        time.sleep(10)
 
 
-
-notions_token = ""
+config.read('~/mobidostat/startup/systemd/creds.ini')
+notions_token = config['notions']['token']
 
 client = NotionClient(token_v2=notions_token)
 page = client.get_block("https://www.notion.so/camdp/RaspberryPis-d8dfbc52736d4bd4b4551cddad2ea38d#2d50a0cdd45443449d02385374f5fc5a")
