06d7e17af96eaedda6bfc748dc7600165989bb4f
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-19 11:55:55 -0400

    monitor

--- morbidostat/actions/take_od_reading.py
+++ morbidostat/actions/take_od_reading.py
@@ -8,23 +8,23 @@ from  paho.mqtt import subscribe, publish
 
 @click.command()
 @click.option('--unit', default="1", help='The morbidostat unit')
-def take_optical_density(unit):
+def take_od_reading(unit):
 
     od_topic = f"morbidostat/{unit}/od_low_pass"
     try:
 
-        click.echo(click.style("starting take_optical_density", fg='green'))
+        click.echo(click.style("starting take_od_reading", fg='green'))
 
         result = subscribe.simple(od_topic, keepalive=10).payload.decode(encoding='UTF-8')
         result = float(result)
 
         click.echo(click.style("   %.3f" % result, fg='yellow'))
-        publish.single(f"morbidostat/{unit}/log", "take_optical_density: %.3fV" % result)
+        publish.single(f"morbidostat/{unit}/log", "take_od_reading: %.3fV" % result)
     except Exception as e:
-        publish.single(f"morbidostat/{unit}/error_log", f"{unit} take_optical_density.py failed with {str(e)}")
-    return
+        publish.single(f"morbidostat/{unit}/error_log", f"{unit} take_od_reading.py failed with {str(e)}")
+    return result
 
 
 
 if __name__ == '__main__':
-    take_optical_density()
+    take_od_reading()
---
+++ morbidostat/long_running/start_monitoring.py
@@ -0,0 +1,44 @@
+"""
+Continuously monitor the mordibodstat and take action. This is the core of the io algorithm
+"""
+import configparser
+import time
+
+import click
+import board
+import busio
+
+from morbidostat.actions.take_od_reading import take_od_reading
+from morbidostat.actions.add_media import add_media
+from morbidostat.actions.remove_waste import remove_waste
+
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+@click.command()
+@click.option('--unit', default="1", help='The morbidostat unit')
+def start_monitoring(unit):
+
+    verbose = True
+    publish.single(f"morbidostat/{unit}/log", "starting start_monitoring.py")
+
+    # first, let's try keeping the culture at a constant OD
+    od_constant = 1.35
+
+    while True:
+        od = take_od_reading()
+        if od > od_constant:
+            publish.single(f"morbidostat/{unit}/log", "monitor triggered IO event.")
+            delta = 0.5
+            remove_waste(delta, unit)
+            add_media(delta, unit)
+
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    start_monitoring()
+
