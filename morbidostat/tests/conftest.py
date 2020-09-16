41827744a0d7cff65701bf13f87d4def2775ef1e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-09-16 13:03:04 -0400

    Tests

--- morbidostat/tests/conftest.py
+++ morbidostat/tests/conftest.py
@@ -1,6 +1,8 @@
 # conftest.py
 
 import pytest
+from morbidostat.utils import config
+
 
 # Replace libraries by fake RPi ones
 import sys
@@ -9,6 +11,7 @@ import fake_rpi
 sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
 sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
 sys.modules['smbus'] = fake_rpi.smbus # Fake smbus (I2C)
+config['network']['leader_hostname'] = "localhost"
 
 
 
--- morbidostat/utils/__init__.py
+++ morbidostat/utils/__init__.py
@@ -1,10 +1,16 @@
+import sys
 import configparser
-import sqlite3
 
+def get_leader_hostname():
+    if "pytest" in sys.modules:
+        return "localhost"
+    else:
+        return get_config()["network"]["leader_hostname"]
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-leader_hostname = config["network"]["leader_hostname"]
+def get_config():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
 
 
 def pump_ml_to_duration(ml, rate, bias):
@@ -18,9 +24,14 @@ def pump_ml_to_duration(ml, rate, bias):
 
 def execute_sql_statement(SQL):
     import pandas as pd
+    import sqlite3
 
     db_location = config["data"]["observation_database"]
     conn = sqlite3.connect(db_location)
     df = pd.read_sql_query(SQL, conn)
     conn.close()
     return df
+
+
+leader_hostname = get_leader_hostname()
+config = get_config()
