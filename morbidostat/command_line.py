db250ca8a5d4bd07fdcf70d18344175038f80004
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-10-07 15:08:09 -0400

    adding background command

--- morbidostat/command_line.py
+++ morbidostat/command_line.py
@@ -7,16 +7,24 @@ import importlib
 
 @click.command(context_settings=dict(ignore_unknown_options=True))
 @click.argument("job")
+@click.option("--background", is_flag=True)
 @click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
-def cli(job, extra_args):
+def cli(job, background, extra_args):
     from subprocess import run, CalledProcessError
 
     extra_args = list(extra_args)
 
     if importlib.util.find_spec(f"morbidostat.background_jobs.{job}"):
-        run(["python3", "-m", f"morbidostat.background_jobs.{job}"] + extra_args)
+        loc = f"morbidostat.background_jobs.{job}"
     else:
-        run(["python3", "-m", f"morbidostat.actions.{job}"] + extra_args)
+        loc = f"morbidostat.actions.{job}"
+
+    command = ["python3", "-m", loc] + extra_args
+
+    if background:
+        command = ["nohup"] + command + ["&"]
+
+    run(command)
 
 
 if __name__ == "__main__":
