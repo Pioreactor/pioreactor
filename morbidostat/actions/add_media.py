57bf5004cafc8ae186f0613bb780923dc7a19cfa
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-10-08 09:02:41 -0400

    debug

--- morbidostat/actions/add_media.py
+++ morbidostat/actions/add_media.py
@@ -36,6 +36,7 @@ def add_media(ml=None, duration=None, duty_cycle=33, verbose=False):
             duration = pump_ml_to_duration(ml, duty_cycle, **loads(config["pump_calibration"][f"media{unit}_ml_calibration"]))
 
         assert duration >= 0
+        print(duration)
         time.sleep(duration)
 
         pwm.stop()
