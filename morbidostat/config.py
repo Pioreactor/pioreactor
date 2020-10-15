cc598281baa072ec2298d594dc26c08764330d37
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-10-15 16:25:56 -0400

    ship .ini in python src folder

--- morbidostat/config.py
+++ morbidostat/config.py
@@ -7,7 +7,7 @@ import sys
 
 def get_config():
     config = configparser.ConfigParser()
-    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../config.ini")
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.ini")
     config.read(config_path)
     return config
 
--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     version="0.1.dev0",
     license="MIT",
     long_description=open("README.md").read(),
-    include_package_data=True,
     install_requires=["click"],
-    package_data={"": ["*.ini"]},
+    include_package_data=True,
+    package_data={"morbidostat": ["config.ini"]},
     packages=find_packages(exclude=["*.tests", "*.tests.*", "*benchmarks*"]),
     entry_points="""
         [console_scripts]
