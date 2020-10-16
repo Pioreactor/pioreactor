3bc422ddd3bf8b3573d3fd4c058f18da907108f0
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-10-16 15:24:54 -0400

    try new pause on od_reading

--- morbidostat/background_jobs/od_reading.py
+++ morbidostat/background_jobs/od_reading.py
@@ -72,7 +72,7 @@ class ODReader:
         self.od_channels = {}
 
         for (label, channel) in od_channels:
-            ai = AnalogIn(ads, getattr(self.ads, "P" + channel))
+            ai = AnalogIn(self.ads, getattr(ADS, "P" + channel))
             self.od_channels[label] = ai
 
         self.pause = 0
