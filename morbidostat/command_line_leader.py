08b6adbd917bac969a07cd9bbb242da4d830c6c2
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-10-21 16:44:51 -0400

    debug

--- morbidostat/command_line_leader.py
+++ morbidostat/command_line_leader.py
@@ -43,15 +43,12 @@ def setup_workers(extra_args):
     s.load_system_host_keys()
 
     for unit in UNITS:
-        print(f"Executing on {unit}...", end=" ")
+        print(f"Executing on {unit}...")
         s.connect(unit, username="pi")
         (stdin, stdout, stderr) = s.exec_command(command)
-        for line in stderr.readlines():
-            pass
         checksum_config_file(s)
         checksum_git(s)
         s.close()
-        print("done")
 
 
 def run_mb_command(job, extra_args):
