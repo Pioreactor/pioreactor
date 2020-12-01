737e29da951970ea8157c337b8ecd03b441eb3a6
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-12-01 10:55:03 -0500

    pre hook and flake improvements

--- pioreactor/background_jobs/growth_rate_calculating.py
+++ pioreactor/background_jobs/growth_rate_calculating.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-import time
 import json
 import os
 import signal
 from collections import defaultdict
-from statistics import median
 
 import numpy as np
 import click
@@ -13,7 +11,7 @@ from pioreactor.utils.streaming_calculations import ExtendedKalmanFilter
 from pioreactor.pubsub import publish, subscribe, subscribe_and_callback, QOS
 
 from pioreactor.whoami import get_unit_from_hostname, get_latest_experiment_name
-from pioreactor.config import config, leader_hostname
+from pioreactor.config import config
 from pioreactor.background_jobs import BackgroundJob
 
 JOB_NAME = os.path.splitext(os.path.basename((__file__)))[0]
@@ -27,7 +25,9 @@ class GrowthRateCalculator(BackgroundJob):
     editable_settings = []
 
     def __init__(self, ignore_cache=False, unit=None, experiment=None, verbose=0):
-        super(GrowthRateCalculator, self).__init__(job_name=JOB_NAME, verbose=verbose, unit=unit, experiment=experiment)
+        super(GrowthRateCalculator, self).__init__(
+            job_name=JOB_NAME, verbose=verbose, unit=unit, experiment=experiment
+        )
         self.ignore_cache = ignore_cache
         self.initial_growth_rate = self.set_initial_growth_rate()
         self.od_normalization_factors = self.set_od_normalization_factors()
@@ -43,26 +43,41 @@ class GrowthRateCalculator(BackgroundJob):
 
     def initialize_extended_kalman_filter(self):
         latest_od = subscribe(f"pioreactor/{self.unit}/{self.experiment}/od_raw_batched")
-        angles_and_initial_points = self.scale_raw_observations(self.json_to_sorted_dict(latest_od.payload))
+        angles_and_initial_points = self.scale_raw_observations(
+            self.json_to_sorted_dict(latest_od.payload)
+        )
 
-        initial_state = np.array([*angles_and_initial_points.values(), self.initial_growth_rate])
+        initial_state = np.array(
+            [*angles_and_initial_points.values(), self.initial_growth_rate]
+        )
 
         d = initial_state.shape[0]
 
         # empirically selected
         initial_covariance = 0.001 * np.diag(initial_state.tolist()[:-1] + [0.001])
 
-        OD_process_covariance = self.create_OD_covariance(angles_and_initial_points.keys())
+        OD_process_covariance = self.create_OD_covariance(
+            angles_and_initial_points.keys()
+        )
 
         rate_process_variance = (0.0025 * self.dt) ** 2
         process_noise_covariance = np.block(
-            [[OD_process_covariance, 0 * np.ones((d - 1, 1))], [0 * np.ones((1, d - 1)), rate_process_variance]]
+            [
+                [OD_process_covariance, 0 * np.ones((d - 1, 1))],
+                [0 * np.ones((1, d - 1)), rate_process_variance],
+            ]
         )
-        observation_noise_covariance = self.create_obs_noise_covariance(angles_and_initial_points.keys())
+        observation_noise_covariance = self.create_obs_noise_covariance(
+            angles_and_initial_points.keys()
+        )
 
         return (
             ExtendedKalmanFilter(
-                initial_state, initial_covariance, process_noise_covariance, observation_noise_covariance, dt=self.dt
+                initial_state,
+                initial_covariance,
+                process_noise_covariance,
+                observation_noise_covariance,
+                dt=self.dt,
             ),
             angles_and_initial_points.keys(),
         )
@@ -76,7 +91,11 @@ class GrowthRateCalculator(BackgroundJob):
 
     def create_OD_covariance(self, angles):
         d = len(angles)
-        variances = {"135": (1e-2 * self.dt) ** 2, "90": (1e-2 * self.dt) ** 2, "45": (1e-2 * self.dt) ** 2}
+        variances = {
+            "135": (1e-2 * self.dt) ** 2,
+            "90": (1e-2 * self.dt) ** 2,
+            "45": (1e-2 * self.dt) ** 2,
+        }
 
         OD_covariance = 0 * np.ones((d, d))
         for i, a in enumerate(angles):
@@ -89,7 +108,11 @@ class GrowthRateCalculator(BackgroundJob):
         if self.ignore_cache:
             return 1
 
-        message = subscribe(f"pioreactor/{self.unit}/{self.experiment}/growth_rate", timeout=2, qos=QOS.EXACTLY_ONCE)
+        message = subscribe(
+            f"pioreactor/{self.unit}/{self.experiment}/growth_rate",
+            timeout=2,
+            qos=QOS.EXACTLY_ONCE,
+        )
         if message:
             return float(message.payload)
         else:
@@ -97,7 +120,11 @@ class GrowthRateCalculator(BackgroundJob):
 
     def set_od_normalization_factors(self):
 
-        message = subscribe(f"pioreactor/{self.unit}/{self.experiment}/od_normalization/median", timeout=2, qos=QOS.EXACTLY_ONCE)
+        message = subscribe(
+            f"pioreactor/{self.unit}/{self.experiment}/od_normalization/median",
+            timeout=2,
+            qos=QOS.EXACTLY_ONCE,
+        )
         if message:
             return self.json_to_sorted_dict(message.payload)
         else:
@@ -106,7 +133,9 @@ class GrowthRateCalculator(BackgroundJob):
     def set_od_variances(self):
 
         message = subscribe(
-            f"pioreactor/{self.unit}/{self.experiment}/od_normalization/variance", timeout=2, qos=QOS.EXACTLY_ONCE
+            f"pioreactor/{self.unit}/{self.experiment}/od_normalization/variance",
+            timeout=2,
+            qos=QOS.EXACTLY_ONCE,
         )
         if message:
             return self.json_to_sorted_dict(message.payload)
@@ -117,7 +146,10 @@ class GrowthRateCalculator(BackgroundJob):
         self.ekf.scale_OD_variance_for_next_n_steps(2e4, 2 * self.samples_per_minute)
 
     def scale_raw_observations(self, observations):
-        return {angle: observations[angle] / self.od_normalization_factors[angle] for angle in observations.keys()}
+        return {
+            angle: observations[angle] / self.od_normalization_factors[angle]
+            for angle in observations.keys()
+        }
 
     def update_state_from_observation(self, message):
         if self.state != self.READY:
@@ -127,11 +159,18 @@ class GrowthRateCalculator(BackgroundJob):
             scaled_observations = self.scale_raw_observations(observations)
             self.ekf.update(list(scaled_observations.values()))
 
-            publish(f"pioreactor/{self.unit}/{self.experiment}/growth_rate", self.state_[-1], verbose=self.verbose, retain=True)
+            publish(
+                f"pioreactor/{self.unit}/{self.experiment}/growth_rate",
+                self.state_[-1],
+                verbose=self.verbose,
+                retain=True,
+            )
 
             for i, angle_label in enumerate(self.angles):
                 publish(
-                    f"pioreactor/{self.unit}/{self.experiment}/od_filtered/{angle_label}", self.state_[i], verbose=self.verbose
+                    f"pioreactor/{self.unit}/{self.experiment}/od_filtered/{angle_label}",
+                    self.state_[i],
+                    verbose=self.verbose,
                 )
 
             return
@@ -164,16 +203,24 @@ class GrowthRateCalculator(BackgroundJob):
     @staticmethod
     def json_to_sorted_dict(json_dict):
         d = json.loads(json_dict)
-        return {k: float(d[k]) for k in sorted(d, reverse=True) if not k.startswith("180")}
+        return {
+            k: float(d[k]) for k in sorted(d, reverse=True) if not k.startswith("180")
+        }
 
 
 def growth_rate_calculating(verbose, ignore_cache):
     try:
-        calculator = GrowthRateCalculator(verbose=verbose, ignore_cache=ignore_cache, unit=unit, experiment=experiment)
+        calculator = GrowthRateCalculator(  # noqa: F841
+            verbose=verbose, ignore_cache=ignore_cache, unit=unit, experiment=experiment
+        )
         while True:
             signal.pause()
     except Exception as e:
-        publish(f"pioreactor/{unit}/{experiment}/error_log", f"[{JOB_NAME}]: failed {e}.", verbose=verbose)
+        publish(
+            f"pioreactor/{unit}/{experiment}/error_log",
+            f"[{JOB_NAME}]: failed {e}.",
+            verbose=verbose,
+        )
 
 
 @click.command()
