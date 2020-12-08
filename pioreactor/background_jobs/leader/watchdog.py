51a1263c70b68f9959ba96fbfef2aa4d8a5eade6
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-12-07 21:23:05 -0500

    adding leader watch dog service

---
+++ pioreactor/background_jobs/leader/watchdog.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import os, signal
+import logging
+
+import click
+
+from pioreactor.whoami import get_unit_from_hostname, UNIVERSAL_EXPERIMENT
+from pioreactor.background_jobs.base import BackgroundJob
+from pioreactor.pubsub import subscribe_and_callback
+
+JOB_NAME = os.path.splitext(os.path.basename((__file__)))[0]
+logger = logging.getLogger(JOB_NAME)
+
+unit = get_unit_from_hostname()
+
+
+class WatchDog(BackgroundJob):
+    def __init__(self, unit, experiment):
+        super(WatchDog, self).__init__(
+            job_name=JOB_NAME, unit=unit, experiment=experiment
+        )
+
+        self.start_passive_listeners()
+
+    def watch_for_lost_state(self, msg):
+        if msg.payload.decode() == self.LOST:
+            # raise the alarms
+            pass
+
+    def watch_for_disk_space_percent(self, msg):
+        if float(msg.payload) >= 90:
+            # raise the alarms
+            pass
+
+    def start_passive_listeners(self):
+        self.pubsub_clients.append(
+            subscribe_and_callback(
+                self.watch_for_lost_state,
+                "pioreactor/+/+/monitor/$state",
+                job_name=self.job_name,
+            )
+        )
+        self.pubsub_clients.append(
+            subscribe_and_callback(
+                self.watch_for_disk_space_percent,
+                "pioreactor/+/+/monitor/disk_space_percent",
+                job_name=self.job_name,
+            )
+        )
+
+
+@click.command(name="watchdog")
+def click_watchdog():
+    """
+    Start the watchdog on the leader
+    """
+    heidi = WatchDog(  # noqa: F841
+        unit=get_unit_from_hostname(), exp=UNIVERSAL_EXPERIMENT
+    )
+
+    signal.pause()
