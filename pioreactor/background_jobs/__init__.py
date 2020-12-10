0ef0c04297f630e74d41703cc125a50bf9efadea
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-12-10 14:58:37 -0500

    add here; kinda annoying

--- pioreactor/background_jobs/__init__.py
+++ pioreactor/background_jobs/__init__.py
@@ -3,6 +3,7 @@ from pioreactor.background_jobs import growth_rate_calculating
 from pioreactor.background_jobs import io_controlling
 from pioreactor.background_jobs import od_reading
 from pioreactor.background_jobs import stirring
+from pioreactor.background_jobs import monitor
 from pioreactor.background_jobs.leader import log_aggregating
 from pioreactor.background_jobs.leader import mqtt_to_db_streaming
 from pioreactor.background_jobs.leader import time_series_aggregating
@@ -16,4 +17,5 @@ __all__ = (
     log_aggregating,
     mqtt_to_db_streaming,
     time_series_aggregating,
+    monitor,
 )
