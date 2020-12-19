14596c32b78377ef86b1dc02d4ab455d903cca85
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-12-19 16:33:21 -0500

    remove this for debugging

--- pioreactor/background_jobs/io_controlling.py
+++ pioreactor/background_jobs/io_controlling.py
@@ -368,27 +368,32 @@ class PIDTurbidostat(IOAlgorithm):
         )
 
     def execute(self, *args, **kwargs) -> events.Event:
-        if self.latest_od <= self.min_od:
-            return events.NoEvent(
-                f"current OD, {self.latest_od:.2f}, less than OD to start diluting, {self.min_od:.2f}"
-            )
-        else:
-            output = self.pid.update(self.latest_od, dt=self.duration)
-
-            volume_to_cycle = output * self.volume
-
-            if volume_to_cycle < 0.01:
+        try:
+            if self.latest_od <= self.min_od:
                 return events.NoEvent(
-                    f"PID output={output:.2f}, so practically no volume to cycle"
+                    f"current OD, {self.latest_od:.2f}, less than OD to start diluting, {self.min_od:.2f}"
                 )
             else:
-                self.execute_io_action(media_ml=volume_to_cycle, waste_ml=volume_to_cycle)
-                e = events.DilutionEvent(
-                    f"PID output={output:.2f}, volume to cycle={volume_to_cycle:.2f}mL"
-                )
-                e.volume_to_cycle = volume_to_cycle
-                e.pid_output = output
-                return e
+                output = self.pid.update(self.latest_od, dt=self.duration)
+
+                volume_to_cycle = output * self.volume
+
+                if volume_to_cycle < 0.01:
+                    return events.NoEvent(
+                        f"PID output={output:.2f}, so practically no volume to cycle"
+                    )
+                else:
+                    self.execute_io_action(
+                        media_ml=volume_to_cycle, waste_ml=volume_to_cycle
+                    )
+                    e = events.DilutionEvent(
+                        f"PID output={output:.2f}, volume to cycle={volume_to_cycle:.2f}mL"
+                    )
+                    e.volume_to_cycle = volume_to_cycle
+                    e.pid_output = output
+                    return e
+        except Exception as e:
+            self.logger.debug(e, exc_info=True)
 
     @property
     def min_od(self):
