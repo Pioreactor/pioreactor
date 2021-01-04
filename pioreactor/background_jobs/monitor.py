b882aecddb816568dc20d8ff98728b2253ef17d7
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-01-03 23:47:46 -0500

    led is controlled by software

--- pioreactor/background_jobs/monitor.py
+++ pioreactor/background_jobs/monitor.py
@@ -9,14 +9,16 @@ import RPi.GPIO as GPIO
 from pioreactor.whoami import get_unit_name, UNIVERSAL_EXPERIMENT
 from pioreactor.background_jobs.base import BackgroundJob
 from pioreactor.utils.timing import RepeatedTimer
-from pioreactor.pubsub import publish, QOS
+from pioreactor.pubsub import publish, QOS, subscribe_and_callback
 from pioreactor.config import config
 
 JOB_NAME = os.path.splitext(os.path.basename((__file__)))[0]
 BUTTON_PIN = config.getint("rpi_pins", "tactile_button")
+LED_PIN = config.getint("rpi_pins", "led")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 
 class Monitor(BackgroundJob):
@@ -24,12 +26,20 @@ class Monitor(BackgroundJob):
         super(Monitor, self).__init__(job_name=JOB_NAME, unit=unit, experiment=experiment)
         self.disk_usage_timer = RepeatedTimer(
             6 * 60 * 60,
-            self.get_and_publish_disk_space,
+            self.publish_disk_space,
             job_name=self.job_name,
             run_immediately=True,
         )
 
         GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=self.button_down_and_up)
+
+        self.start_passive_listeners()
+
+    def turn_on_led(self):
+        GPIO.output(LED_PIN, GPIO.HIGH)
+
+    def turn_off_led(self):
+        GPIO.output(LED_PIN, GPIO.LOW)
 
     def button_down_and_up(self):
         # TODO: test
@@ -38,26 +48,30 @@ class Monitor(BackgroundJob):
             1,
             qos=QOS.AT_LEAST_ONCE,
         )
+
+        self.turn_on_led()
+
         publish(
             f"pioreactor/{self.unit}/{self.experiment}/{self.job_name}/log",
             "Pushed tactile button",
         )
+
         while GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            # we keep sending it because they may change the webpage.
+
+            # we keep sending it because the user may change the webpage.
             publish(
-                f"pioreactor/{self.unit}/{self.experiment}/{self.job_name}/button_down",
-                1,
-                qos=QOS.AT_LEAST_ONCE,
+                f"pioreactor/{self.unit}/{self.experiment}/{self.job_name}/button_down", 1
             )
             time.sleep(0.25)
 
+        self.turn_off_led()
         publish(
             f"pioreactor/{self.unit}/{self.experiment}/{self.job_name}/button_down",
             0,
             qos=QOS.AT_LEAST_ONCE,
         )
 
-    def get_and_publish_disk_space(self):
+    def publish_disk_space(self):
         import psutil
 
         disk_usage_percent = psutil.disk_usage("/").percent
@@ -69,6 +83,46 @@ class Monitor(BackgroundJob):
         publish(
             f"pioreactor/{self.unit}/{self.experiment}/{self.job_name}/disk_usage_percent",
             disk_usage_percent,
+        )
+
+    def flicker_led(self, _):
+        # TODO: what happens when I hear multiple msgs in quick succession?
+        self.turn_on_led()
+        time.sleep(0.2)
+        self.turn_off_led()
+        time.sleep(0.2)
+        self.turn_on_led()
+        time.sleep(0.2)
+        self.turn_off_led()
+
+        time.sleep(0.5)
+
+        self.turn_on_led()
+        time.sleep(0.2)
+        self.turn_off_led()
+        time.sleep(0.2)
+        self.turn_on_led()
+        time.sleep(0.2)
+        self.turn_off_led()
+
+        time.sleep(0.5)
+
+        self.turn_on_led()
+        time.sleep(0.2)
+        self.turn_off_led()
+        time.sleep(0.2)
+        self.turn_on_led()
+        time.sleep(0.2)
+        self.turn_off_led()
+
+    def start_passive_listeners(self):
+
+        self.pubsub_clients.append(
+            subscribe_and_callback(
+                self.flicker_led,
+                f"pioreactor/{self.unit}/{self.experiment}/{self.job}/flicker_led",
+                job_name=self.job_name,
+            )
         )
 
 
