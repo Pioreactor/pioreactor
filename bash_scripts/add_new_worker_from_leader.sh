87f8f0c36e258d31fd7cdd8000c441b0a6e07cce
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-01-13 12:24:19 -0500

    add new pioreactor

--- bash_scripts/add_new_worker_from_leader.sh
+++ bash_scripts/add_new_worker_from_leader.sh
@@ -7,11 +7,11 @@ ssh-keygen -R $1.local
 ssh-keygen -R raspberrypi.local
 
 # allow us to SSH in
-sshpass -p 'raspberry' ssh -o StrictHostKeyChecking=no raspberrypi.local 'mkdir .ssh'
+sshpass -p 'raspberry' ssh -o StrictHostKeyChecking=no raspberrypi.local 'mkdir -p .ssh'
 cat ~/.ssh/id_rsa.pub | sshpass -p 'raspberry' ssh -o StrictHostKeyChecking=no raspberrypi.local 'cat >> .ssh/authorized_keys'
 
 # install worker onto Rpi
-ssh -o StrictHostKeyChecking=no -o raspberrypi.local 'wget -O install_pioreactor_as_worker.sh https://gist.githubusercontent.com/CamDavidsonPilon/08aa165a283fb7af7262e4cb598bf6a9/raw/install_pioreactor_as_worker.sh && bash ./install_pioreactor_as_worker.sh $1'
+ssh -o StrictHostKeyChecking=no raspberrypi.local 'wget -O install_pioreactor_as_worker.sh https://gist.githubusercontent.com/CamDavidsonPilon/08aa165a283fb7af7262e4cb598bf6a9/raw/install_pioreactor_as_worker.sh && bash ./install_pioreactor_as_worker.sh $1'
 
 # add to known hosts
 ssh-keyscan -H $1 >> ~/.ssh/known_hosts
