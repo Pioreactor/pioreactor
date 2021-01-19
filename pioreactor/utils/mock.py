bfaaa9ddddb58207401d8f9ad2916a58bd3767f7
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-01-19 10:52:22 -0500

    custom formatter for error msgs to mqtt; hiding Mock OD stuff

---
+++ pioreactor/utils/mock.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# mock pieces for testing
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
+class MockI2C:
+    def __init__(self, SCL, SDA):
+        pass
+
+    def writeto(self, *args, **kwargs):
+        return
+
+    def try_lock(self, *args, **kwargs):
+        return True
+
+    def unlock(self, *args, **kwargs):
+        pass
+
+
+class MockAnalogIn(AnalogIn):
+    STATE = 0.2
+
+    @property
+    def voltage(self):
+        import random
+
+        self.STATE = self.STATE * random.lognormvariate(0.15 * 5 / 60 / 60, 0.0005)
+        return self.STATE
