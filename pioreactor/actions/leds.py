a78c67943be750341e49b023bbbe94d2f44837f9
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-01-26 17:30:11 -0500

    wip on leds

---
+++ pioreactor/actions/leds.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import logging
+
+logger = logging.getLogger("led_intensity")
+
+
+def led_intensity(channel=None, intensity=0.0):
+    assert 0 <= intensity <= 100
+    assert channel in ["A", "B", "C", "D"]
+    try:
+
+        return True
+    except Exception as e:
+        logger.debug(e, exc_info=True)
+        logger.error(e)
+        return False
