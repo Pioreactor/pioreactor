2c75b62edcddb6a823c8be7d75ef95957e5f26ec
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-01-27 16:34:28 -0500

    testing out new led control

--- pioreactor/background_jobs/led_control.py
+++ pioreactor/background_jobs/led_control.py
@@ -20,7 +20,7 @@ from pioreactor.pubsub import QOS
 from pioreactor.utils import pio_jobs_running
 from pioreactor.whoami import get_unit_name, get_latest_experiment_name
 from pioreactor.background_jobs.base import BackgroundJob
-from pioreactor.background_jobs.subjob.led_algorithm import Silent
+from pioreactor.background_jobs.subjobs.led_algorithm import Silent
 
 
 class LEDController(BackgroundJob):
