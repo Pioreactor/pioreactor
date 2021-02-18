b772ebe3150e7bbb2e913b3f84cbf519fd8b056b
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-02-18 16:49:23 -0500

    adding gli2 as an test

--- pioreactor/actions/gli2.py
+++ pioreactor/actions/gli2.py
@@ -34,14 +34,25 @@ def gli2(pd_X, pd_Y, led_X, led_Y, unit=None, experiment=None):
                led_Y
 
     """
+
+    def update_intensity(channel, intensity):
+        led_intensity(
+            channel,
+            intensity=intensity,
+            verbose=False,
+            source_of_event="gli2",
+            unit=unit,
+            experiment=experiment,
+        )
+
     assert "od_reading" not in pio_jobs_running(), "Turn off od_reading job first."
 
     adc = ADCReader(unit=unit, experiment=experiment, dynamic_gain=False)
     adc.setup_adc()
 
     # reset all to 0
-    led_intensity(led_X, intensity=0, verbose=False, source_of_event="gli2")
-    led_intensity(led_Y, intensity=0, verbose=False, source_of_event="gli2")
+    update_intensity(led_X, 0)
+    update_intensity(led_Y, 0)
 
     # take baseline measurements
     adc.take_reading()
@@ -51,45 +62,45 @@ def gli2(pd_X, pd_Y, led_X, led_Y, unit=None, experiment=None):
     # find values of LED intensity s.t. we don't overload the 180 degree sensor
     # X first
     for i in range(1, 100):
-        led_intensity(led_X, intensity=i, verbose=False, source_of_event="gli2")
+        update_intensity(led_X, i)
         adc.take_reading()
         if getattr(adc, f"A{pd_X}") >= 2.048:
             X_max = i - 1
-            led_intensity(led_X, 0, verbose=False, source_of_event="gli2")
+            update_intensity(led_X, 0)
             break
     else:
         X_max = 100
 
     # Y next
     for i in range(1, 100):
-        led_intensity(led_Y, intensity=i, verbose=False, source_of_event="gli2")
+        update_intensity(led_Y, i)
         adc.take_reading()
         if getattr(adc, f"A{pd_Y}") >= 2.048:
             Y_max = i - 1
-            led_intensity(led_Y, 0, verbose=False, source_of_event="gli2")
+            update_intensity(led_Y, 0)
             break
     else:
         Y_max = 100
 
     def make_measurement():
-        led_intensity(led_Y, intensity=0, verbose=False, source_of_event="gli2")
-        led_intensity(led_X, intensity=X_max, verbose=False, source_of_event="gli2")
+        update_intensity(led_Y, 0)
+        update_intensity(led_X, X_max)
 
         adc.take_reading()
         signal1 = (getattr(adc, f"A{pd_Y}") - baselineY) / (
             getattr(adc, f"A{pd_X}") - baselineX
         )
 
-        led_intensity(led_X, intensity=0, verbose=False, source_of_event="gli2")
-        led_intensity(led_Y, intensity=Y_max, verbose=False, source_of_event="gli2")
+        update_intensity(led_X, 0)
+        update_intensity(led_Y, Y_max)
 
         adc.take_reading()
         signal2 = (getattr(adc, f"A{pd_X}") - baselineX) / (
             getattr(adc, f"A{pd_Y}") - baselineY
         )
 
-        led_intensity(led_X, intensity=0, verbose=False, source_of_event="gli2")
-        led_intensity(led_Y, intensity=0, verbose=False, source_of_event="gli2")
+        update_intensity(led_X, 0)
+        update_intensity(led_Y, 0)
         return sqrt(signal1 * signal2)
 
     signal = make_measurement()
