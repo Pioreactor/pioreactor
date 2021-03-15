00872a9a9f93f000e6eaa821ff1c5f90b56a746b
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-03-15 14:03:25 -0400

    download -> export

--- pioreactor/actions/__init__.py
+++ pioreactor/actions/__init__.py
@@ -4,12 +4,12 @@ from pioreactor.actions import add_media
 from pioreactor.actions import remove_waste
 from pioreactor.actions import od_normalization
 from pioreactor.actions import led_intensity
-from pioreactor.actions.leader import download_experiment_data
+from pioreactor.actions.leader import export_experiment_data
 from pioreactor.actions.leader import backup_database
 
 
 __all__ = (
-    download_experiment_data,
+    export_experiment_data,
     backup_database,
     od_normalization,
     remove_waste,
