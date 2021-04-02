c33e561636461c90123907eeada3331611f2308d
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-04-01 21:27:02 -0400

    play with params

--- pioreactor/background_jobs/growth_rate_calculating.py
+++ pioreactor/background_jobs/growth_rate_calculating.py
@@ -244,7 +244,7 @@ class GrowthRateCalculator(BackgroundJob):
 
         # an improvement to this: the variance factor is proportional to the amount exchanged.
         self.update_ekf_variance_after_event(
-            minutes=0.5, factor=15 / config.getfloat("growth_rate_kalman", "od_variance")
+            minutes=0.5, factor=100 / config.getfloat("growth_rate_kalman", "od_variance")
         )
 
     def start_passive_listeners(self):
