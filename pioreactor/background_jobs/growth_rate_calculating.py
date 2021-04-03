fc2dcdf779417a6e107b2db5576fe5892d9b0529
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-04-02 20:49:05 -0400

    hardcode this, otherwise I was getting div-by-0

--- pioreactor/background_jobs/growth_rate_calculating.py
+++ pioreactor/background_jobs/growth_rate_calculating.py
@@ -243,9 +243,7 @@ class GrowthRateCalculator(BackgroundJob):
             return
 
         # an improvement to this: the variance factor is proportional to the amount exchanged.
-        self.update_ekf_variance_after_event(
-            minutes=1, factor=10 / config.getfloat("growth_rate_kalman", "od_variance")
-        )
+        self.update_ekf_variance_after_event(minutes=1, factor=10000)
 
     def start_passive_listeners(self):
         # process incoming data
