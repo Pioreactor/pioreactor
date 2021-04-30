acb32752b06fde97219de203dc2e19282b8ed706
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-04-30 10:57:55 -0400

    try this - this is kinda hacky that I'm using a different topic, but this won't spoil the main experiment data

--- pioreactor/actions/od_blank.py
+++ pioreactor/actions/od_blank.py
@@ -39,10 +39,8 @@ def od_blank(od_angle_channel=None, unit=None, experiment=None, N_samples=30):
 
     def yield_from_mqtt():
         while True:
-            msg = pubsub.subscribe(
-                f"pioreactor/{unit}/{experiment}-blank/od_raw_batched",
-                allow_retained=False,
-            )
+            msg = pubsub.subscribe(f"pioreactor/{unit}/{experiment}-blank/od_raw_batched")
+            print(msg)
             yield json.loads(msg.payload)
 
     signal = yield_from_mqtt()
@@ -54,6 +52,7 @@ def od_blank(od_angle_channel=None, unit=None, experiment=None, N_samples=30):
             for (sensor, reading) in batched_reading.items():
                 readings[sensor].append(reading)
 
+            print(count)
             if count == N_samples:
                 break
 
