e2fc0f085f1e2480ce94d21d525ae2225ddc322a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-05-03 10:50:28 -0400

    improved pio version; adding coverted timestamp cols to data exports

--- pioreactor/actions/leader/export_experiment_data.py
+++ pioreactor/actions/leader/export_experiment_data.py
@@ -33,8 +33,8 @@ def generate_timestamp_to_localtimestamp_clause(cursor, table_name):
     )
     if clause:
         clause += ","
-    else:
-        return ""
+
+    return clause
 
 
 def export_experiment_data(experiment, output, tables):
