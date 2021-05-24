62046b08b06462ec81cd50c39bd34399d229f023
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-05-24 08:44:48 -0400

    add example custom dosing auto as a script

---
+++ experiments/example_of_custom_dosing_automation.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import signal
+from pioreactor.background_jobs.subjobs.dosing_automation import DosingAutomation
+from pioreactor.background_jobs.dosing_control import DosingController
+from pioreactor.whoami import get_unit_name, get_latest_experiment_name
+
+
+class NaiveTurbidostat(DosingAutomation):
+
+    key = "naive_turbidostat"
+
+    def __init__(self, target_od, **kwargs):
+        super(NaiveTurbidostat, self).__init__(**kwargs)
+        self.target_od = target_od
+
+    def execute(self):
+        if self.latest_od > self.target_od:
+            self.execute_io_action(media_ml=1.0, waste_ml=1.0)
+
+
+if __name__ == "__main__":
+
+    doser = DosingController(
+        "pid_morbidostat",
+        target_od=2.0,
+        unit=get_unit_name(),
+        experiment=get_latest_experiment_name(),
+    )
+
+    while True:
+        signal.pause()
