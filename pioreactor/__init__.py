6f42e7edfb5cba8094f26dde0bf079cfa328000a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-06-13 07:56:44 -0400

    some cleanup; test our demandimport

--- pioreactor/__init__.py
+++ pioreactor/__init__.py
@@ -7,13 +7,23 @@ except ImportError:  # TODO: this is available in 3.8+
 from collections import namedtuple
 
 from pioreactor.version import __version__  # noqa: F401
-from pioreactor.background_jobs import *  # noqa: F401,F403
+from pioreactor.background_jobs import (  # noqa: F401,F403
+    growth_rate_calculating,  # noqa: F401,F403
+    dosing_control,  # noqa: F401,F403
+    led_control,  # noqa: F401,F403
+    temperature_control,  # noqa: F401,F403
+    od_reading,  # noqa: F401,F403
+    stirring,  # noqa: F401,F403
+    monitor,  # noqa: F401,F403
+    mqtt_to_db_streaming,  # noqa: F401,F403
+    watchdog,  # noqa: F401,F403
+)  # noqa: F401,F403
 
 # from pioreactor.actions import *  # noqa: F401,F403
 
 # needed to import to "load" the automation subclasses,
 # and hence the *-controller will register them.
-from pioreactor.automations import *  # noqa: F401,F403
+from pioreactor.automations import dosing, led, temperature, events  # noqa: F401,F403
 
 
 def get_plugins():
