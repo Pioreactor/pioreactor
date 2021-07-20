9a616aafc977ca144468f68d33a3c6d09c576c7e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-07-20 18:34:54 -0400

    thinking more about this...

--- pioreactor/background_jobs/od_reading.py
+++ pioreactor/background_jobs/od_reading.py
@@ -280,7 +280,7 @@ class ADCReader(BackgroundSubJob):
                             0,
                             0.80 / (oversampling_count - 1)
                             - delta()  # the delta() reduces the variance by accounting for the duration of each sampling.
-                            + 0.000
+                            + 0.005
                             * (
                                 (counter * 0.618034) % 1
                             ),  # this is to artificially spread out the samples, so that we observe less aliasing.
