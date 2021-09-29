c71ecf8357a155ce269ed2f8ba2f6f2ba0c036c4
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-09-29 15:39:52 -0400

    taylor this

--- pioreactor/actions/stirring_calibration.py
+++ pioreactor/actions/stirring_calibration.py
@@ -41,7 +41,7 @@ def stirring_calibration():
 
         # we go up and down to exercise any hysteresis in the system
         # dcs = list(range(55, 97, 4)) + list(range(95, 50, -4))
-        dcs = list(range(30, 60, 4)) + list(range(63, 30, -4))
+        dcs = list(range(40, 60, 4)) + list(range(63, 40, -4))
 
         measured_rpms = []
 
