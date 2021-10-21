808ada1a4851edb13b9a4e5085850a54781a477a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-10-21 08:59:11 -0400

    support for 1-off python scripts as plugins

--- pioreactor/__init__.py
+++ pioreactor/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
+import glob, importlib, os, pathlib, sys
 
 try:
     from importlib.metadata import entry_points, metadata
@@ -18,12 +19,17 @@ def __getattr__(attr):
         raise AttributeError
 
 
-def get_plugins():
+Plugin = namedtuple("Plugin", ["module", "description", "version", "homepage"])
+
+
+def get_plugins() -> dict[str, Plugin]:
     """
     This function is really time consuming...
     """
-    Plugin = namedtuple("Plugin", ["module", "description", "version", "homepage"])
 
+    # get entry point plugins
+    # Users can use Python's entry point system to create rich plugins, see
+    # example here: https://github.com/Pioreactor/pioreactor-air-bubbler
     eps = entry_points()
     pioreactor_plugins = eps.get("pioreactor.plugins", [])
     plugins = {}
@@ -35,4 +41,34 @@ def get_plugins():
             )
         except Exception as e:
             print(f"{plugin.name} plugin load error: {e}")
+
+    # get file-based plugins.
+    # Users can put .py files into the MODULE_DIR folder below.
+    # The below code will load it into Python, and treat it like any other plugin.
+    # The authors can add metadata to their file with the following variables at the
+    # highest level in the file:
+    # __name__
+    # __summary__
+    # __version__
+    # __homepage__
+
+    # The directory containing your modules needs to be on the search path.
+    MODULE_DIR = "/home/pi/.pioreactor/plugins"
+    sys.path.append(MODULE_DIR)
+
+    # Get the stem names (file name, without directory and '.py') of any
+    # python files in your directory, load each module by name and run
+    # the required function.
+    py_files = glob.glob(os.path.join(MODULE_DIR, "*.py"))
+
+    for py_file in py_files:
+        module_name = pathlib.Path(py_file).stem
+        module = importlib.import_module(module_name)
+        plugins[getattr(module, "__name__", module_name)] = Plugin(
+            module,
+            getattr(module, "__summary__", None),
+            getattr(module, "__version__", None),
+            getattr(module, "__homepage__", None),
+        )
+
     return plugins
