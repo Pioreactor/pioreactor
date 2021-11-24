531a80dffefd23be455fd32f18dd5dc16d5607d3
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-11-24 10:49:29 -0500

    y

--- pioreactor/cli/pio.py
+++ pioreactor/cli/pio.py
@@ -203,7 +203,7 @@ def update(ui: bool, app: bool) -> None:
         """
         # for now, let's just update to master to make my life easier
         latest_release_version = "master"
-        command = "sudo pip3 uninstall pioreactor && sudo pip3 install https://github.com/pioreactor/pioreactor/archive/master.zip"
+        command = "sudo pip3 uninstall -y pioreactor && sudo pip3 install https://github.com/pioreactor/pioreactor/archive/master.zip"
         p = subprocess.run(
             command,
             shell=True,
