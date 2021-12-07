551c9854257f4c25ef51b7601b3caa0d9dd0370e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-12-07 09:06:59 -0500

    try this hack

--- pioreactor/tests/conftest.py
+++ pioreactor/tests/conftest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import pytest
 
 # Replace libraries by fake RPi ones
 import sys
@@ -10,3 +11,18 @@ sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
 sys.modules["smbus"] = fake_rpi.smbus  # Fake smbus (I2C)
 
 fake_rpi.toggle_print(False)
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    from pioreactor.utils import local_intermittent_storage
+
+    with local_intermittent_storage("pio_jobs_running") as cache:
+        for key in cache.keys():
+            del cache[key]
+
+    with local_intermittent_storage("led_locks") as cache:
+        for key in cache.keys():
+            del cache[key]
+
+    yield
