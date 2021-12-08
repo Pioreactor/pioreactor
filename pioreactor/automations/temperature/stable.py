3991a8842fc466b23d3fe07bcdd0f593615b4d8e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-12-08 15:35:14 -0500

    smaller starting point

--- pioreactor/automations/temperature/stable.py
+++ pioreactor/automations/temperature/stable.py
@@ -65,7 +65,7 @@ class Stable(TemperatureAutomation):
                 self.update_heater(0)
             else:
                 self.update_heater(
-                    delta_t * 3.5
+                    delta_t * 2.5
                 )  # TODO: provide a better linear estimate here, also will fail if using an external PSU
             return  # we'll update with the PID on the next loop.
 
