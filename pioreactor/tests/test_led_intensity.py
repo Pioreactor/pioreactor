5e53b3bb5abebadac49fd95592a50282f4c2817b
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-01-05 13:18:07 -0500

    Nones were showing up in mqtt, ex: pioreactor/None/None/led/C/intensity, and this is why

--- pioreactor/tests/test_led_intensity.py
+++ pioreactor/tests/test_led_intensity.py
@@ -6,6 +6,7 @@ from pioreactor.actions.led_intensity import (
     led_intensity,
     LED_Channel,
 )
+from pioreactor.whoami import get_unit_name, get_latest_experiment_name
 from pioreactor.utils import local_intermittent_storage
 
 
@@ -13,10 +14,15 @@ def test_lock_will_prevent_led_from_updating() -> None:
 
     channel: LED_Channel = "A"
 
-    assert led_intensity(channels=channel, intensities=20)
+    unit = get_unit_name()
+    exp = get_latest_experiment_name()
+
+    assert led_intensity(channels=channel, intensities=20, unit=unit, experiment=exp)
 
     with lock_leds_temporarily([channel]):
-        assert not led_intensity(channels=channel, intensities=10)
+        assert not led_intensity(
+            channels=channel, intensities=10, unit=unit, experiment=exp
+        )
 
     with local_intermittent_storage("leds") as cache:
         assert float(cache[channel]) == 20
@@ -24,14 +30,21 @@ def test_lock_will_prevent_led_from_updating() -> None:
 
 def test_lock_will_prevent_led_from_updating_single_channel_but_not_others_passed_in() -> None:
 
-    assert led_intensity(channels=["A", "B"], intensities=[20, 20])
+    unit = get_unit_name()
+    exp = get_latest_experiment_name()
+
+    assert led_intensity(
+        channels=["A", "B"], intensities=[20, 20], unit=unit, experiment=exp
+    )
 
     with local_intermittent_storage("leds") as cache:
         assert float(cache["B"]) == 20
         assert float(cache["A"]) == 20
 
     with lock_leds_temporarily(["A"]):
-        assert not led_intensity(channels=["A", "B"], intensities=[10, 10])
+        assert not led_intensity(
+            channels=["A", "B"], intensities=[10, 10], unit=unit, experiment=exp
+        )
 
     with local_intermittent_storage("leds") as cache:
         assert float(cache["B"]) == 10
@@ -39,20 +52,26 @@ def test_lock_will_prevent_led_from_updating_single_channel_but_not_others_passe
 
 
 def test_error_is_thrown_if_lengths_are_wrong() -> None:
+    unit = get_unit_name()
+    exp = get_latest_experiment_name()
 
     with pytest.raises(RuntimeError):
-        led_intensity(channels=["A", "B"], intensities=[20])
+        led_intensity(channels=["A", "B"], intensities=[20], unit=unit, experiment=exp)
 
     with pytest.raises(RuntimeError):
-        led_intensity(channels=["A", "B"], intensities=20)
+        led_intensity(channels=["A", "B"], intensities=20, unit=unit, experiment=exp)
 
-    assert led_intensity(channels=["A"], intensities=20)
+    assert led_intensity(channels=["A"], intensities=20, unit=unit, experiment=exp)
 
 
 def test_local_cache_is_updated() -> None:
 
     channel: LED_Channel = "B"
-    assert led_intensity(channels=channel, intensities=20)
+
+    unit = get_unit_name()
+    exp = get_latest_experiment_name()
+
+    assert led_intensity(channels=channel, intensities=20, unit=unit, experiment=exp)
 
     with local_intermittent_storage("leds") as cache:
         assert float(cache["B"]) == 20
