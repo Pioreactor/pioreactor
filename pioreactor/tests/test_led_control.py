d5f1d798d32703d63d54fe29398f6bbedd2d6eb2
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-02-08 09:28:48 -0500

    more tests

--- pioreactor/tests/test_led_control.py
+++ pioreactor/tests/test_led_control.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 # testing_led_control
-import pytest
-import time, json
+from __future__ import annotations
 
-from pioreactor.background_jobs.led_control import LEDController
-from pioreactor.automations.led.base import LEDAutomation
-from pioreactor.whoami import get_unit_name, get_latest_experiment_name
+import json
+import time
+
+import pytest
+
 from pioreactor import pubsub
-from pioreactor.actions.led_intensity import lock_leds_temporarily, LED_UNLOCKED
+from pioreactor.actions.led_intensity import LED_UNLOCKED
+from pioreactor.actions.led_intensity import lock_leds_temporarily
+from pioreactor.automations.led.base import LEDAutomation
+from pioreactor.background_jobs.led_control import LEDController
 from pioreactor.utils import local_intermittent_storage
+from pioreactor.whoami import get_unit_name
 
 unit = get_unit_name()
-experiment = get_latest_experiment_name()
 
 
 def pause() -> None:
@@ -20,6 +24,7 @@ def pause() -> None:
 
 
 def test_silent() -> None:
+    experiment = "test_silent"
     with LEDController("silent", duration=60, unit=unit, experiment=experiment):
         pause()
         pause()
@@ -42,8 +47,13 @@ def test_silent() -> None:
 
 
 def test_changing_automation_over_mqtt() -> None:
-    with LEDController("silent", duration=60, unit=unit, experiment=experiment) as ld:
-
+    experiment = "test_changing_automation_over_mqtt"
+    original_duration = 60
+    with LEDController(
+        "silent", duration=original_duration, unit=unit, experiment=experiment
+    ) as ld:
+        assert ld.automation_name == "silent"
+        assert ld.automation["duration"] == original_duration
         pause()
         pause()
         r = pubsub.subscribe(
@@ -55,15 +65,17 @@ def test_changing_automation_over_mqtt() -> None:
         pause()
         pubsub.publish(
             f"pioreactor/{unit}/{experiment}/led_control/automation/set",
-            '{"automation_name": "silent", "duration": "20"}',
+            json.dumps({"automation_name": "silent", "duration": 20}),
         )
         pause()
         pause()
         pause()
         pause()
         pause()
+        pause()
+        pause()
         assert ld.automation_name == "silent"
-        assert ld.automation["duration"] == "20"
+        assert ld.automation["duration"] == 20
 
 
 @pytest.mark.xfail
@@ -71,6 +83,7 @@ def test_we_respect_any_locks_on_leds_we_want_to_modify() -> None:
     """
     This test works locally, but not in github CI
     """
+    experiment = "test_we_respect_any_locks_on_leds_we_want_to_modify"
     with local_intermittent_storage("led_locks") as cache:
         cache["A"] = LED_UNLOCKED
         cache["B"] = LED_UNLOCKED
