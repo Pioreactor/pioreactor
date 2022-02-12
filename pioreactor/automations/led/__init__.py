28de760067dc19de6a9c74c425aec652536fa6c4
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-02-11 20:30:13 -0500

    clean up old cod

--- pioreactor/automations/led/__init__.py
+++ pioreactor/automations/led/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import annotations
 
-import time
-from pioreactor.automations.led.base import LEDAutomation
 from pioreactor.automations import events
-from pioreactor.config import config
+from pioreactor.automations.led.base import LEDAutomation
 
 
 class Silent(LEDAutomation):
@@ -15,46 +14,3 @@ class Silent(LEDAutomation):
 
     def execute(self) -> events.Event:
         return events.NoEvent("no changes occur in Silent")
-
-
-class TrackOD(LEDAutomation):
-    """
-    max_od: float
-        the theoretical maximum (normalized) OD we expect to see.
-
-    """
-
-    automation_name = "track_od"
-
-    def __init__(self, max_od: float, **kwargs) -> None:
-        super(TrackOD, self).__init__(**kwargs)
-        assert max_od is not None, "max_od should be set"
-        self.max_od = max_od
-        self.white_light = config.get(
-            "leds", "white_light"
-        )  # TODO: update to new led config.ini syntax
-        self.set_led_intensity(self.white_light, 0)
-
-    def execute(self) -> events.Event:
-        if self.latest_od is not None:
-            new_intensity = 100 ** (min(self.latest_od, self.max_od) / self.max_od)
-            self.set_led_intensity(self.white_light, new_intensity)
-            return events.ChangedLedIntensity(f"new output: {new_intensity}")
-        else:
-            return events.NoEvent()
-
-
-class FlashUV(LEDAutomation):
-
-    automation_name = "flash_uv"
-
-    def __init__(self, **kwargs) -> None:
-        super(FlashUV, self).__init__(**kwargs)
-        self.uv_led = config.get("leds_reverse", "uv")
-        self.set_led_intensity(self.uv_led, 0)
-
-    def execute(self) -> events.Event:
-        self.set_led_intensity(self.uv_led, 100)
-        time.sleep(1)
-        self.set_led_intensity(self.uv_led, 0)
-        return events.ChangedLedIntensity("Flashed UV for 1 second")
