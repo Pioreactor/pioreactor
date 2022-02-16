05f516b0ce0633f0c85f4869d81973cca0ba20dd
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-02-16 16:55:42 -0500

    simple test to confirm that the sin regression's penalizer is scale indp.

--- pioreactor/tests/test_od_reading.py
+++ pioreactor/tests/test_od_reading.py
@@ -177,3 +177,29 @@ def test_error_thrown_if_wrong_angle() -> None:
         "135", "90", fake_data=True, experiment="test_error_thrown_if_wrong_angle"
     )
     st.set_state(st.DISCONNECTED)
+
+
+def test_sin_regression_penalizer_C_is_independent_of_scale_of_observed_values() -> None:
+
+    freq = 60
+    C_True = 10
+    x = [i / 25 for i in range(25)]
+    y = [C_True + 2 * np.sin(freq * 2 * np.pi * _x) + 0.1 * np.random.randn() for _x in x]
+
+    adc_reader = ADCReader(channels=[])
+
+    (C, A, phi), _ = adc_reader.sin_regression_with_known_freq(
+        x, y, freq, prior_C=12, penalizer_C=10.0
+    )
+    ratio = C / C_True
+
+    # scale everything by 10
+    factor = 10
+    y = [factor * y_ for y_ in y]
+
+    adc_reader = ADCReader(channels=[])
+
+    (C, A, phi), _ = adc_reader.sin_regression_with_known_freq(
+        x, y, freq, prior_C=factor * 12, penalizer_C=10.0
+    )
+    assert np.abs(C / (factor * C_True) - ratio) < 0.01
