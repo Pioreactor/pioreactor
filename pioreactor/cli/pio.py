df03b789231667b3880ac339fcc1f905f1f03230
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-09-20 12:43:34 -0400

    test new update

--- pioreactor/cli/pio.py
+++ pioreactor/cli/pio.py
@@ -302,11 +302,11 @@ def update(ui: bool, app: bool, branch: Optional[str], source: Optional[str]) ->
     if app:
         if source is not None:
             version_installed = source
-            command = f"sudo pip3 install --root-user-action=ignore --disable-pip-version-check -U --force-reinstall {source}"
+            command = f"sudo pip3 install --root-user-action=ignore -U --force-reinstall {source}"
 
         elif branch is not None:
             version_installed = branch
-            command = f"sudo pip3 install --root-user-action=ignore --disable-pip-version-check -U --force-reinstall https://github.com/pioreactor/pioreactor/archive/{branch}.zip"
+            command = f"sudo pip3 install --root-user-action=ignore -U --force-reinstall https://github.com/pioreactor/pioreactor/archive/{branch}.zip"
         else:
             latest_release_metadata = loads(
                 get("https://api.github.com/repos/pioreactor/pioreactor/releases/latest").body
@@ -314,7 +314,7 @@ def update(ui: bool, app: bool, branch: Optional[str], source: Optional[str]) ->
             version_installed = latest_release_metadata["name"]
             url_to_get_whl = f"https://github.com/Pioreactor/pioreactor/releases/download/{version_installed}/pioreactor-{version_installed}-py3-none-any.whl"
 
-            command = f'sudo pip3 install --root-user-action=ignore --disable-pip-version-check "pioreactor @ {url_to_get_whl}"'
+            command = f'sudo pip3 install --root-user-action=ignore "pioreactor @ {url_to_get_whl}"'
 
         p = subprocess.run(
             command,
@@ -329,18 +329,16 @@ def update(ui: bool, app: bool, branch: Optional[str], source: Optional[str]) ->
             logger.error(p.stderr)
 
     if ui and whoami.am_I_leader():
-        cd = "cd ~/pioreactorui/backend"
         gitp = "git pull origin master"
-        npm_install = "npm install"
-        setup = "pm2 restart ui"
-        unedit_edited_files = "git checkout ."  # TODO: why do I do this. Can I be more specific than `.`? This blocks edits to the contrib folder from sticking around.
-        command = " && ".join([cd, gitp, setup, npm_install, unedit_edited_files])
+        restart_lighttp = "sudo systemctl restart lighttp"
+        command = " && ".join([gitp, restart_lighttp])
         p = subprocess.run(
             command,
             shell=True,
             universal_newlines=True,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.PIPE,
+            cwd="/var/www/pioreactorui/",
         )
         if p.returncode == 0:
             logger.notice("Updated PioreactorUI to latest version.")  # type: ignore
@@ -427,16 +425,17 @@ if whoami.am_I_leader():
 
         # check to make sure X.local is on network
         checks, max_checks = 0, 20
+        sleep_time = 3
         while not networking.is_hostname_on_network(hostname_dot_local):
             checks += 1
             try:
                 socket.gethostbyname(hostname_dot_local)
             except socket.gaierror:
-                sleep(3)
+                sleep(sleep_time)
                 click.echo(f"`{hostname}` not found on network - checking again.")
                 if checks >= max_checks:
                     logger.error(
-                        f"`{hostname}` not found on network after {max_checks} seconds. Check that you provided the right WiFi credentials to the network, and that the Raspberry Pi is turned on."
+                        f"`{hostname}` not found on network after more than {max_checks * sleep_time} seconds. Check that you provided the right WiFi credentials to the network, and that the Raspberry Pi is turned on."
                     )
                     sys.exit(1)
 
