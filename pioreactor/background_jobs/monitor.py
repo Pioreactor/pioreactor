9e3f5e6a8fc81e4d4314b395cbee4030efa2446f
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-12-22 17:43:12 -0500

    fill in firmware version

--- pioreactor/background_jobs/monitor.py
+++ pioreactor/background_jobs/monitor.py
@@ -89,6 +89,10 @@ class Monitor(BackgroundJob):
             f"Pioreactor HAT version: {pretty_version(version.hardware_version_info)}"
         )
 
+        self.logger.debug(
+            f"Pioreactor firmware version: {pretty_version(version.get_firmware_version())}"
+        )
+
         self.logger.debug(f"Pioreactor HAT serial number: {version.serial_number}")
 
         self.button_down = False
