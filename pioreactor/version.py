fadf8a0643554b3836df7494923fabe99a467cc4
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-01-05 11:01:01 -0500

    append .dev

--- pioreactor/version.py
+++ pioreactor/version.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import os
 
 # Append ".dev" if a dev version
-__version__ = "23.1.0"
+__version__ = "23.1.0.dev"
 
 
 def _get_hardware_version() -> tuple[int, int] | tuple[int, int, str]:
