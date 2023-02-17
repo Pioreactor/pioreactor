377eb060fa0f1576969cd3a7a3958676634d0bc1
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-02-17 14:54:27 -0500

    rip and ship

--- pioreactor/version.py
+++ pioreactor/version.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import os
 
 # Append ".dev" if a dev version
-__version__ = "23.2.8.dev"
+__version__ = "23.2.17"
 
 
 def _get_hardware_version() -> tuple[int, int] | tuple[int, int, str]:
