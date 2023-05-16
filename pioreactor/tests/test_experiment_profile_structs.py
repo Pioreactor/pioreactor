efb9b24674269a0d279f86d7ec49e764fd8e2918
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-05-16 09:57:16 -0400

    changelog and more examples

--- pioreactor/tests/test_experiment_profile_structs.py
+++ pioreactor/tests/test_experiment_profile_structs.py
@@ -81,6 +81,34 @@ pioreactors:
     assert decode(file, type=structs.Profile) is not None
 
 
+def test_simple3():
+    file = """
+experiment_profile_name: simple_stirring_example
+
+metadata:
+  author: John Doe
+  description: A simple example of a stirring job in a single pioreactor
+
+aliases:
+  reactor_1: PR-001
+
+common:
+  stirring:
+    actions:
+      - type: start
+        hours_elapsed: 0.0
+        parameters:
+          target_rpm: 200.0
+      - type: stop
+        hours_elapsed: 2.0
+
+pioreactors:
+  reactor_1:
+    jobs: {}
+"""
+    assert decode(file, type=structs.Profile) is not None
+
+
 def test_complex():
     file = """
 experiment_profile_name: multi_jobs_dependencies
@@ -157,5 +185,118 @@ pioreactors:
               nitrogen_rate: 10
           - type: stop
             hours_elapsed: 5.0
+"""
+    assert decode(file, type=structs.Profile) is not None
+
+
+def test_complex2():
+    file = """
+experiment_profile_name: multi_bioreactor_complex
+
+metadata:
+  author: Jane Doe
+  description: Complex experiment with multiple jobs and bioreactors
+
+aliases:
+  bioreactor_A: BR-001
+  bioreactor_B: BR-002
+
+common:
+  stirring:
+    actions:
+      - type: start
+        hours_elapsed: 0.0
+        options:
+          target_rpm: 200.0
+      - type: stop
+        hours_elapsed: 4.0
+  od_reading:
+    actions:
+      - type: start
+        hours_elapsed: 0.0
+      - type: stop
+        hours_elapsed: 4.0
+  growth_rate_calculating:
+    actions:
+      - type: start
+        hours_elapsed: 0.5
+      - type: stop
+        hours_elapsed: 3.5
+
+pioreactors:
+  bioreactor_A:
+    jobs:
+      dosing_control:
+        actions:
+          - type: start
+            hours_elapsed: 1.0
+            options:
+              automation_name: turbidostat
+              volume: 1.0
+              target_normalized_od: 5.0
+          - type: stop
+            hours_elapsed: 3.0
+"""
+    assert decode(file, type=structs.Profile) is not None
+
+
+def test_complex3():
+    file = """
+experiment_profile_name: multi_bioreactor_very_complex
+
+metadata:
+  author: Alex Doe
+  description: Very complex experiment with multiple jobs and bioreactors, different jobs on different bioreactors
+
+aliases:
+  bioreactor_A: BR-001
+  bioreactor_B: BR-002
+  bioreactor_C: BR-003
+
+common:
+  stirring:
+    actions:
+      - type: start
+        hours_elapsed: 0.0
+        options:
+          target_rpm: 200.0
+      - type: stop
+        hours_elapsed: 4.0
+  od_reading:
+    actions:
+      - type: start
+        hours_elapsed: 0.0
+      - type: stop
+        hours_elapsed: 4.0
+
+pioreactors:
+  bioreactor_A:
+    jobs:
+      dosing_control:
+        actions:
+          - type: start
+            hours_elapsed: 1.0
+            options:
+              automation_name: chemostat
+              volume: 1.0
+              duration: 10
+          - type: stop
+            hours_elapsed: 3.0
+  bioreactor_B:
+    jobs:
+      growth_rate_calculating:
+        actions:
+          - type: start
+            hours_elapsed: 0.5
+          - type: stop
+            hours_elapsed: 3.5
+      add_media:
+        actions:
+          - type: start
+            hours_elapsed: 2.0
+            options:
+              volume: 10.0
+          - type: stop
+            hours_elapsed: 2.5
 """
     assert decode(file, type=structs.Profile) is not None
