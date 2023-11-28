5242e0f04766ef4b416a074299ce9828a51cc0c0
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-11-28 10:03:28 -0500

    fix args

--- pioreactor/background_jobs/monitor.py
+++ pioreactor/background_jobs/monitor.py
@@ -623,7 +623,7 @@ class Monitor(BackgroundJob):
             options["experiment"] = whoami._get_latest_experiment_name()  # techdebt
             Thread(
                 target=utils.boolean_retry,
-                args=(led_intensity, state),
+                args=(led_intensity, (state,)),
                 kwargs=options,
             ).start()
 
