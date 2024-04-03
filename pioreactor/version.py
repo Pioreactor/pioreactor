443cc7bb6dc09f79a8fda971844e749f8a039709
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2024-04-03 11:00:03 -0400

    bump version

--- pioreactor/version.py
+++ pioreactor/version.py
@@ -7,7 +7,7 @@ import os
 # Append ".dev0" if a dev version
 # Append "rc0" if a rc version
 # No zero padding!
-__version__ = "24.3.10.dev0"
+__version__ = "24.4.3rc0"
 
 
 def get_hardware_version() -> tuple[int, int] | tuple[int, int, str]:
