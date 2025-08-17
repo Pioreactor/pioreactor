5b6bd28c975726b38c894d34e5662d866253d15b
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2025-08-16 21:53:24 -0400

    using /run/pioreactor instead of tmp. Moving contrib ui stuff to .pioreactor

--- core/pioreactor/web/fcgi.py
+++ core/pioreactor/web/fcgi.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 
 from flup.server.fcgi import WSGIServer
+from pioreactor.web import tasks  # noqa: F401
 from pioreactor.web.app import create_app
-from pioreactor.web.tasks import tasks  # noqa: F401
 
 
 def main():
