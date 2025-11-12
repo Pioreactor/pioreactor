7b70b7efa3848f63cb4d5782ef267819a72f9665
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2025-11-12 15:50:38 -0500

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-### Upcoming
+### 25.11.12
 
 #### Highlights
 
--- core/pioreactor/version.py
+++ core/pioreactor/version.py
@@ -7,7 +7,7 @@ import os
 # Append ".dev0" if a dev version
 # Append "rc0" if a rc version
 # No zero padding!
-__version__ = "25.11.10rc0"
+__version__ = "25.11.12"
 
 
 def get_hardware_version() -> tuple[int, int] | tuple[int, int, str] | None:
