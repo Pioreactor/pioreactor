922df3608f7188fc5cf9be503a677f3c74ec0330
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-01-26 13:11:27 -0500

    bump rc version

--- core/pioreactor/version.py
+++ core/pioreactor/version.py
@@ -5,7 +5,7 @@ import os
 # Append ".dev0" if a dev version
 # Append "rc0" if a rc version
 # No zero padding!
-__version__ = "26.1.16rc0"
+__version__ = "26.1.26rc0"
 
 
 def get_hardware_version() -> tuple[int, int] | tuple[int, int, str] | None:
