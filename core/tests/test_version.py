1b6722d79db146f38b2cbae63cd425e99e208c5e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-03-12 13:17:31 -0400

    last test

--- core/tests/test_version.py
+++ core/tests/test_version.py
@@ -5,4 +5,4 @@ from pioreactor.version import __version__
 def test_no_zero_padding() -> None:
     parts = __version__.split(".")
     for p in parts:
-        assert not p.startswith("0")
+        assert p == "0" or not p.startswith("0")
