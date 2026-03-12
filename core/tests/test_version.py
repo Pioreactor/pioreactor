3a50fd4e24bd75833c304c50ebce62bc16db6318
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-03-12 14:17:13 -0400

    fix test

--- core/tests/test_version.py
+++ core/tests/test_version.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import re
+
 from pioreactor.version import __version__
 
 
 def test_no_zero_padding() -> None:
-    parts = __version__.split(".")
-    for p in parts:
+    parts = re.match(r"^(\d+\.\d+\.\d+)", __version__)
+    assert parts is not None
+
+    release_parts = parts.group(1).split(".")
+    for p in release_parts:
         assert p == "0" or not p.startswith("0")
