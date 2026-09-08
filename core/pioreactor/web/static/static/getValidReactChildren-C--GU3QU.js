adfe323df9a3213b2b166776fcb04e7c909812fc
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-09-08 09:37:18 -0400

    bump rc version

---
+++ core/pioreactor/web/static/static/getValidReactChildren-C--GU3QU.js
@@ -0,0 +1,1 @@
+import{r}from"./index-Dw4Lkjyn.js";function a(t){return r.Children.toArray(t).filter(e=>r.isValidElement(e))}export{a as g};
---
+++ core/update_scripts/26.9.0rc1/update.sql
@@ -0,0 +1,11 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS experiment_chart_preferences (
+    experiment TEXT NOT NULL PRIMARY KEY,
+    overview_chart_keys TEXT,
+    pioreactor_chart_keys TEXT,
+    FOREIGN KEY (experiment) REFERENCES experiments (
+        experiment
+    ) ON DELETE CASCADE
+);
+PRAGMA busy_timeout = 15000;
